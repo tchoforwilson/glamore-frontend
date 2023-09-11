36b180162ebbd045f912e5ae6c80340fb9670de9
tchoforwilson <tchoforwilson@gmail.com>
2023-09-11 13:04:54 +0100

    changed

---
+++ src/components/buttons/SubmitButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useFormikContext } from "formik";
+import Spinner from "../common/spinner";
+
+/**
+ * @breif Render a submit button
+ * @param {String} title Title of submit button
+ * @param {Boolean} loading Loading property
+ * @returns {JSX}
+ */
+const SubmitButton = ({ title, loading, ...otherProps }) => {
+  const { handleSubmit } = useFormikContext();
+  return (
+    <button
+      type="submit"
+      onClick={handleSubmit}
+      disabled={loading}
+      className="btn btn--secondary btn-submit"
+      {...otherProps}
+    >
+      {loading ? <Spinner className="spinner spinner-sm" /> : title}
+    </button>
+  );
+};
+
+export default SubmitButton;
---
+++ src/components/common/spinner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Spinner = ({ ...otherProps }) => {
+  return <div {...otherProps}></div>;
+};
+
+export default Spinner;
