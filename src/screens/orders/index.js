046807d074d39c74f198ddbe8b7fe0b158765db1
Dodley <dodleydesign+github@gmail.com>
2023-09-12 13:58:53 +0100

    Made the base scaffold for dodley branch

---
+++ src/screens/orders/index.js
@@ -0,0 +1,9 @@
+import {Container} from '@mui/material'
+import {default as nn} from './orders.module.css'
+
+
+export default function Orders(){
+    return <Container>
+        <p>This is the orders page</p>
+    </Container>
+}
