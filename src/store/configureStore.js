b4e6ac7d20dc440136c120469dbd9d24a5bb3aaa
tchoforwilson <tchoforwilson@gmail.com>
2023-09-12 09:10:22 +0100

    Added toastify to store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { apiRequest, logger, Toastify } from "./middlewares";
+import { apiRequest, logger, toastify } from "./middlewares";
 import reducer from "./reducer";
 
 const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(logger, Toastify, apiRequest),
+    getDefaultMiddleware().concat(logger, apiRequest, toastify),
 });
 
 export default store;
