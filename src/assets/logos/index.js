51abe4024505aa3b7f52d93d61d523c1a5793de9
tchoforwilson <tchoforwilson@gmail.com>
2023-09-12 09:18:50 +0100

    Deleted

--- src/assets/logos/index.js
+++
@@ -1,4 +0,0 @@
-export const logo = {
-    logo_l: require('./logo_l.png'),
-    logo_t: require('./logo_t.jpg'),
-  };
--- src/assets/mockup/index.js
+++
@@ -1,6 +0,0 @@
-export const mockup = {
-    img1: require('./layer1.png'),
-    img2: require('./layer2.png'),
-    img3: require('./layer3.png'),
-    lastimg: require('./lastlayer.png'),
-  };
