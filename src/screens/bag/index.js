74ea5b0321eb8f92dc1e6e7718c7b2c5e27613e7
Dodley <dodleydesign+github@gmail.com>
2023-09-13 00:52:13 +0100

    Added more volume to the bag component

--- src/screens/bag/index.js
+++ src/screens/bag/index.js
@@ -1,8 +1,10 @@
 import { Box, Container, Grid } from "@mui/material";
 import { default as nn } from "./bag.module.css";
-import { Add, Remove } from "@mui/icons-material";
+import { Add, Remove, Delete } from "@mui/icons-material";
+import { useState } from "react";
+import img1 from "../../assets/moreimg/img1.jpg";
 
-const AddRemoveButton = ({ type }) => {
+const AddRemoveButton = ({ type, add, remove }) => {
   /**
    * Render a small add or subtract button
    *
@@ -11,7 +13,17 @@ const AddRemoveButton = ({ type }) => {
    */
 
   return (
-    <button className={nn.addRemove_button}>
+    <button
+      onClick={() => {
+        if (type === "add") {
+          add();
+        } else {
+          remove();
+        }
+      }}
+      type="button"
+      className={nn.addRemove_button}
+    >
       {type === "add" ? <Add /> : <Remove />}
     </button>
   );
@@ -28,36 +40,75 @@ const BagItem = ({ itemName, quantity, price, discount, size }) => {
     @return {JSX}
     */
 
-  const discountedPrice = price - (price * discount/100);
+  const getInitialQuantity = () => quantity;
+
+  const [qty, setquantity] = useState(getInitialQuantity());
+
+  const add = () => {
+    setquantity((prev) => (prev < 99 ? prev + 1 : 99));
+    return null;
+  };
+  const remove = () => {
+    setquantity((prev) => (prev > 0 ? prev - 1 : 0));
+    return null;
+  };
+  const discountedPrice = price - (price * discount) / 100;
   return (
-    <Grid container columns={10} rowSpacing={2}>
+    <Grid container columns={10} sx={{ padding: "1rem 0" }}>
       <Grid item xs={3}>
-        Image Section
+        <img src={img1} className={nn.item_image} />
       </Grid>
       <Grid item xs={6}>
         <Box
           sx={{
             display: "flex",
             flexDirection: "column",
-            gap: "1rem"
+            gap: "1rem",
           }}
         >
           <div>
             <p>{itemName || "Joggers White Signed T-shirt"}</p>
           </div>
-          <Box sx={{ display: "flex", gap: "1.5rem", alignItems: "center"}}>
-            <AddRemoveButton type="add" />
-            1
-            <AddRemoveButton type="remove" />{" "}
+          <Box sx={{ display: "flex", gap: "1.5rem", alignItems: "center" }}>
+            <AddRemoveButton type="add" add={add} />
+            <input
+              value={qty}
+              type="number"
+              className={nn.item_input}
+              onChange={(e) => {
+                const num = Number(e.target.value);
+                if (typeof num === "number" && !isNaN(num)) {
+                  if (num >= 0 && num <= 99) {
+                    setquantity(num);
+                  }
+                } else {
+                  setquantity(0);
+                }
+              }}
+            />
+            <AddRemoveButton type="remove" remove={remove} />{" "}
           </Box>
-          <Box sx={{ display: "flex", alignItems: "center", gap: "12px"}}>
-            {discount ? <span>{discountedPrice}</span> : null}
-            <span className={`${discount ? nn.price_discount + " " + nn.item: nn.item}`}>{price}</span>
+          <Box sx={{ display: "flex", alignItems: "center", gap: "12px" }}>
+            {discount ? (
+              <span className={nn.discount_price}>XAF {discountedPrice}</span>
+            ) : null}
+            <span className={`${discount ? nn.old_price : null}`}>
+              XAF {price}
+            </span>
           </Box>
         </Box>
       </Grid>
       <Grid item xs={1}>
-        Size and action
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+          }}
+        >
+          <span className={nn.item_size}>XXL</span>
+          <Delete className={nn.delete_icon} />
+        </Box>
       </Grid>
     </Grid>
   );
@@ -79,12 +130,86 @@ export default function Bag() {
         {/* Mobile topbar here */}
         {/* Header here */}
         <div className={nn.bagItem_container}>
-          <BagItem itemName="Joggers White Signed T-shirt" quantity={4} price={700} discount={2} />
-          <BagItem itemName="Joggers White Signed T-shirt" quantity={4} price={700} discount={2} />
-          <BagItem itemName="Joggers White Signed T-shirt" quantity={4} price={700} discount={2} />
-          <BagItem itemName="Joggers White Signed T-shirt" quantity={4} price={700} discount={2} />
-          <BagItem itemName="Joggers White Signed T-shirt" quantity={4} price={700} discount={2} />
-          <BagItem itemName="Joggers White Signed T-shirt" quantity={4} price={700} discount={2} />
+          <BagItem
+            itemName="Joggers White Signed T-shirt"
+            quantity={4}
+            price={700}
+            discount={2}
+          />
+          <BagItem
+            itemName="Joggers White Signed T-shirt"
+            quantity={4}
+            price={700}
+            discount={2}
+          />
+          <BagItem
+            itemName="Joggers White Signed T-shirt"
+            quantity={4}
+            price={700}
+            discount={2}
+          />
+          <BagItem
+            itemName="Joggers White Signed T-shirt"
+            quantity={4}
+            price={700}
+            discount={2}
+          />
+          <BagItem
+            itemName="Joggers White Signed T-shirt"
+            quantity={4}
+            price={700}
+            discount={2}
+          />
+          <BagItem
+            itemName="Joggers White Signed T-shirt"
+            quantity={4}
+            price={700}
+            discount={2}
+          />
+          <Box sx={{margin: "2rem 0"}}>
+            <Box sx={{ display: "flex", alignItems: "center", gap: '1rem' }}>
+            
+              <button type="button" className={nn.promo_btn}>
+                Insert Promo Code
+              </button>
+              <button type="button" className={nn.apply_btn}>
+                Apply
+              </button>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <p>Sub total</p>
+              <p className={nn.item_total}>XAF 2,800</p>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <p>Delivery</p>
+              <p className={nn.item_total}>XAF 800</p>
+            </Box>
+          </Box>
+        </div>
+        <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <p>Total</p>
+              <p className={nn.item_total}>XAF 3,600</p>
+            </Box>
+        <div>
+          {/* <CheckoutButton /> */}
         </div>
       </Box>
     </Container>
