586df064d2148f1d65302107f8f21b349dcfbbe5
Dodley <dodleydesign+github@gmail.com>
2023-09-12 14:53:20 +0100

    Created layout for Bag

--- src/screens/bag/index.js
+++ src/screens/bag/index.js
@@ -1,8 +1,15 @@
-import {Container} from '@mui/material'
+import {Box, Container} from '@mui/material'
 import {default as nn} from './bag.module.css'
 
 export default function Bag(){
-    return <Container>
-    <p>This is the bag</p>
+    return <Container disableGutters={true} maxWidth="xl">
+        <Box sx={{display: {xs: 'none', md:'block'}}}>
+            This is desktop
+        </Box>
+        <Box sx={{display: {
+            xs: "block", md: "none"
+        }, height: "100%"}}>
+            This is mobile
+        </Box>
     </Container>
 }
