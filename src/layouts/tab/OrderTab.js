b36e1917db3493a2e8dc1323905d624b7b5dbf91
tchoforwilson <tchoforwilson@gmail.com>
2023-09-16 19:57:11 +0100

    Order tab implementation

--- src/layouts/tab/OrderTab.js
+++ src/layouts/tab/OrderTab.js
@@ -1,30 +1,26 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
 import { SearchURLContext } from "../../contexts";
 import { SearchWidget } from "../../components/searchwiget";
 
-const items = [
-  { title: "new orders", to: "/new-orders" },
-  { title: "all orders", to: "/all-orders" },
-  { title: "important", to: "/important" },
-  { title: "completed", to: "/completed" },
-];
+const items = ["new orders", "all orders", "important", "completed"];
 
-const OrderTab = () => {
-  const location = useLocation();
-  const pathname = location.pathname.split("/")[1];
+const OrderTab = ({ onTabClick }) => {
+  const [activeTab, sectActiveTab] = useState("new orders");
+  const handleTabclick = (tab) => {
+    sectActiveTab(tab);
+    onTabClick(tab);
+  };
   return (
     <div className="order-tab">
       <div className="order-tab__menu">
-        {items.map((item, index) => (
-          <Link
-            className={`order-tab__item ${
-              pathname === item.to ? "active" : ""
-            }`}
-            to={item.to}
+        {items.map((item) => (
+          <span
+            key={item}
+            className={`order-tab__item ${activeTab === item ? "active" : ""}`}
+            onClick={handleTabclick}
           >
-            {item.title}
-          </Link>
+            {item}
+          </span>
         ))}
       </div>
       <SearchURLContext.Provider value={{ searchUrl: "orders" }}>
