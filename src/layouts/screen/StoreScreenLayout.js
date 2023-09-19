d8ce2e3c8d4f6e6acd42a5786b2b1625608f7c08
tchoforwilson <tchoforwilson@gmail.com>
2023-09-19 14:29:00 +0100

    Store screen layout component

---
+++ src/layouts/screen/StoreScreenLayout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import { SideBar } from "../sidebar";
+
+/**
+ * @breif This is the layout nature of user store screen.
+ * Sidebar on the left, header on top and content below.
+ * @param {HTML} children Children elements
+ * @returns {JSX}
+ */
+const StoreScreenLayout = ({ children }) => {
+  return (
+    <Stack sx={{ height: "100vh" }} direction="row" spacing={5}>
+      <Box sx={{ width: "20%", height: "100vh" }}>
+        <SideBar />
+      </Box>
+      <Box sx={{ width: "80%", height: "100vh", padding: "2rem" }}>
+        {children}
+      </Box>
+    </Stack>
+  );
+};
+
+export default StoreScreenLayout;
