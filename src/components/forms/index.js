e72ad0bb983a95ef8e438cbfd21281b6ad205e58
tchoforwilson <tchoforwilson@gmail.com>
2023-09-20 20:56:23 +0100

    Exported FormFieldLabel

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,3 +1,4 @@
 export { default as FormContainer } from "./FormContainer";
 export { default as FormField } from "./FormField";
+export { default as FormFieldLabel } from "./FormFieldLabel";
 export { default as FormErrorMessage } from "./FormErrorMessage";
