2167a9d8a2755c262bf752b99ad6d24d1ac7a722
tchoforwilson <tchoforwilson@gmail.com>
2023-09-20 20:56:02 +0100

    Refactored form field component

--- src/components/forms/FormField.js
+++ src/components/forms/FormField.js
@@ -3,12 +3,20 @@ import { useFormikContext } from "formik";
 import { AppInput } from "../inputs";
 import FormErrorMessage from "./FormErrorMessage";
 
+/**
+ * @breif Render a form field with border bottom only and field
+ * @param {String} name Form field name
+ * @param {String} placeholder Form field placeholder
+ * @param {String} type Form field type
+ * @returns {HTML} div
+ */
 const FormField = ({ name, placeholder, type }) => {
   const { values, errors, touched, setFieldValue, setFieldTouched } =
     useFormikContext();
   return (
     <div className="form__group">
       <AppInput
+        id={name}
         type={type}
         name={name}
         value={values[name]}
