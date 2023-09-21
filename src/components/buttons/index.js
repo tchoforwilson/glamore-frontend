b69c69f658ea8f713226c146bf3f40d929ef5008
anyepeter <petzyrockchendi@gmail.com>
2023-09-21 18:06:07 +0100

    restructure the codes base on the requirements

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,4 +1,4 @@
 export { default as AppButton } from "./AppButton";
 export { default as SubmitButton } from "./SubmitButton";
 export { default as BackButton } from "./BackButton";
-export { default as EditProfile } from "./editProfile";
+export { default as EditProfile } from "../../screens/user-profile/editProfile";
