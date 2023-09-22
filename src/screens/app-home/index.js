d4f2ad456fd04035e107f9d83f52c6bbe30544a9
tchoforwilson <tchoforwilson@gmail.com>
2023-09-22 05:57:03 +0100

    App home screen

--- src/screens/app-home/index.js
+++ src/screens/app-home/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import NavBar from "../../layouts/navbar";
+import { NavBar } from "../../layouts";
 import { SearchURLContext } from "../../contexts";
 
 const AppHome = () => {
