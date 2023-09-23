f0dfc5f6cac8dd1129f20737a881bcc315f491a7
tchoforwilson <tchoforwilson@gmail.com>
2023-09-23 09:12:20 +0100

    Table body component for all tables

---
+++ src/components/tables/TableBody.js
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import _ from "lodash";
+import { TableContext } from "../../contexts";
+
+/**
+ * @breif Render a table body with
+ * @param {Array} data  Array of data to be displayed on table body
+ * @param {Array} columns Array of columns of the table data
+ * @returns {HTMLElement} tablebody
+ */
+const TableBody = () => {
+  /**
+   * @breif Render table cell data
+   * @param {Object} item Object to be rendered
+   * @param {String} column Object field name as columns
+   * @returns {String}
+   */
+  const renderCell = (item, column) => {
+    if (column.content) return column.content(item);
+    return _.get(item, column.path);
+  };
+
+  /**
+   * @breif Create row key
+   * @param {Object} item Object to be rendered
+   * @param {String} column Object field name as columns
+   * @returns {String}
+   */
+  const createKey = (item, column) => item._id + (column.path || column.key);
+
+  /**
+   * @breif Get data from table context
+   */
+  const { data, columns } = useContext(TableContext);
+  return (
+    <tbody>
+      {data.map((item, index) => (
+        <tr key={item._id || index + 1}>
+          {columns.map((column) => (
+            <td key={createKey(item, column)}>{renderCell(item, column)}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
+};
+
+export default TableBody;
