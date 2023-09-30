06700f6bb425bd217bc6aac658b4e2f8e3914c64
tchoforwilson <tchoforwilson@gmail.com>
2023-09-30 07:25:54 +0100

    Created form text area

---
+++ src/components/forms/FormTextArea.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useFormikContext } from "formik";
+import { AppTextArea } from "../inputs";
+import FormErrorMessage from "./FormErrorMessage";
+
+/**
+ * @breif Render a form text area
+ * @param {String} name Form field name
+ * @param {String} placeholder Form field placeholder
+ * @param {Boolean} isBodered Form field is bordered
+ * @returns {HTML} div
+ */
+const FormTextArea = ({ name, placeholder, isBordered = false }) => {
+  const { values, errors, touched, setFieldValue, setFieldTouched } =
+    useFormikContext();
+  return (
+    <div className="form__group">
+      <AppTextArea
+        id={name}
+        name={name}
+        value={values[name]}
+        placeholder={placeholder}
+        onChange={(e) => setFieldValue(name, e.target.value)}
+        onBlur={() => setFieldTouched(name)}
+        className={`form__textarea ${
+          isBordered ? "form__textarea--bordered" : ""
+        }`}
+      />
+      <FormErrorMessage error={errors[name]} visible={touched[name]} />
+    </div>
+  );
+};
+
+export default FormTextArea;
