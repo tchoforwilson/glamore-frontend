2f072dd0d67d39c44db3596475b294ec6a820519
tchoforwilson <tchoforwilson@gmail.com>
2023-10-12 10:26:26 +0100

    .\src\assets\styles\sass\pages\_preview-store.scss

--- src/screens/preview-store/PreviewStoreScreen.js
+++ src/screens/preview-store/PreviewStoreScreen.js
@@ -1,11 +1,73 @@
 import React, { useMemo } from "react";
-import { AppScreenLayout } from "../../layouts";
 import { useParams } from "react-router-dom";
+import { Container, Grid } from "@mui/material";
+import WestRoundedIcon from "@mui/icons-material/WestRounded";
+import { FollowButton, ShareButton } from "../../components/buttons";
+import { AppScreenLayout } from "../../layouts";
+import { ProductCard } from "../../components/cards";
+
+const product = { name: "Cotton beige T-shirt", price: 700, currency: "XAF" };
 
 const PreviewStoreScreen = () => {
   const { id } = useParams();
   const searchPath = useMemo(() => ({ searchUrl: `/${id}/products` }), [id]);
-  return <AppScreenLayout searchPath={searchPath}></AppScreenLayout>;
+  return (
+    <AppScreenLayout searchPath={searchPath}>
+      <Container>
+        {/* Store Images */}
+        <section className="preview-store__section section-images">
+          <Grid container spacing={1}>
+            <Grid
+              item
+              md={1}
+              sm={1}
+              sx={{ marginTop: "auto", marginBottom: "auto" }}
+            >
+              <div className="preview-store__icon-container">
+                <WestRoundedIcon className="preview-store__icon" />
+              </div>
+            </Grid>
+            <Grid item md={11} sm={11}>
+              <div
+                className="preview-store__background"
+                style={{
+                  backgroundImage: `url(${require("./background.png")})`,
+                }}
+              >
+                <img
+                  src={require("../../assets/images/profile.png")}
+                  className="preview-store__logo"
+                  alt="Store logo"
+                />
+              </div>
+            </Grid>
+          </Grid>
+        </section>
+        {/* Store Information */}
+        <section className="preview-store__section section-store-info">
+          <Grid container sx={{ justifyContent: "center" }}>
+            <Grid item md={4} sm={12} sx={{ textAlign: "center" }}>
+              <h1 className="preview-store__name">Francisca's fashion store</h1>
+              <span className="preview-store__products">83 products</span>
+              <div className="d-flex justify-content--evenly mt-2">
+                <FollowButton />
+                <ShareButton />
+              </div>
+            </Grid>
+          </Grid>
+        </section>
+        {/* Store products */}
+        <section className="preview-store__section section-store-products">
+          <h2 className="preview-store__header">featured products</h2>
+          <div className="preview-store__items">
+            {Array.from({ length: 20 }).map((_, index) => (
+              <ProductCard key={index} product={product} />
+            ))}
+          </div>
+        </section>
+      </Container>
+    </AppScreenLayout>
+  );
 };
 
 export default PreviewStoreScreen;
