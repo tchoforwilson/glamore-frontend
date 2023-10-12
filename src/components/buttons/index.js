9fcf64bbbe1e2c35db222d7ad59ed42f41e722c2
tchoforwilson <tchoforwilson@gmail.com>
2023-10-12 08:08:21 +0100

    Exported Follow and Share button

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,4 +1,6 @@
 export { default as AppButton } from "./AppButton";
-export { default as SubmitButton } from "./SubmitButton";
 export { default as BackButton } from "./BackButton";
+export { default as FollowButton } from "./FollowButton";
+export { default as SubmitButton } from "./SubmitButton";
+export { default as ShareButton } from "./ShareButton";
 export { default as UpgradeButton } from "./UpgradeButton";
