fc628348d8a705cf8b6dc29c46775de350bbb59a
tchoforwilson <tchoforwilson@gmail.com>
2023-10-12 16:53:43 +0100

    Began implementing home screen

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { Container } from "@mui/material";
 import { AppScreenLayout } from "../../layouts";
 
 const HomeScreen = () => {
-  return <AppScreenLayout searchPath="products"></AppScreenLayout>;
+  return (
+    <AppScreenLayout searchPath="products">
+      <Container>
+        <section className="home-sellers">
+          <h1 className="home-header">explore our best sellers</h1>
+        </section>
+        <section className="home-categories">
+          <h1 className="home-header">browse categories</h1>
+        </section>
+        <section className="home-essentials">
+          <h1 className="home-header">the essentials</h1>
+        </section>
+        <section className="home-popular">
+          <h1 className="home-header">popular buys</h1>
+        </section>
+      </Container>
+    </AppScreenLayout>
+  );
 };
 
 export default HomeScreen;
