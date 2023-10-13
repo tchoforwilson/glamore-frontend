65ae103224eca272225c14afd63dd0146ad865ff
tchoforwilson <tchoforwilson@gmail.com>
2023-10-13 16:13:20 +0100

    Values for sorted modal component

---
+++ src/components/modals/values.js
@@ -0,0 +1,61 @@
+export const dates = ["RECENT", "THIS MONTH", "THIS YEAR", "ANYTIME"];
+export const sortBy = ["RATING", "POPULARITY", "FEATURED", "LOCATION"];
+export const brands = ["ADDIDAS", "VOGUE", "LUIS VUITTON", "NIKE"];
+export const genders = ["MEN", "WOMEN", "BOYS", "GIRLS"];
+export const sizes = [
+  "SMALL SIZE",
+  "MEDIUM",
+  "MEDIUM PLUS",
+  "LARGE",
+  "EXTRA LARGE",
+  "XXL",
+];
+
+export const deals = ["COUPONS", "DISCOUNT", "BULK BUYS", "2 FOR 1"];
+export const materials = [
+  "COTTON",
+  "POLYESTER",
+  "SILK FABRIC",
+  "TRADITIONAL",
+  "LEATHER",
+  "FUR",
+  "WOOL",
+];
+
+export const colors = [
+  "navy",
+  "royalblue",
+  "turquoise",
+  "colonial",
+  "frenchblue",
+  "merlot",
+  "plum",
+  "grape",
+  "lavender",
+  "periwinkle",
+  "mulbery",
+  "fuschia",
+  "tearose",
+  "strawberry",
+  "lipstick",
+  "blush",
+  "breath",
+  "nude",
+  "peach",
+  "chick",
+  "leaf",
+  "robin's egg",
+  "sky",
+  "frost",
+  "terra",
+  "chocolate",
+  "fox",
+  "fawn",
+  "camel",
+  "khaki",
+  "black",
+  "dove",
+  "stone",
+  "metallic gold",
+  "metallic silver",
+];
