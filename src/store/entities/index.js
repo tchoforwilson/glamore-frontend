8b266b944f11cf09a2d98e4205c5af1199d224f6
tchoforwilson <tchoforwilson@gmail.com>
2023-10-16 20:11:29 +0100

    exported reducers

--- src/store/entities/index.js
+++ src/store/entities/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from "redux";
+import categoriesReducer from "./categories";
+import storesReducer from "./stores";
 import productsReducer from "./products";
+import usersReducer from "./users";
+
 export default combineReducers({
+  categories: categoriesReducer,
+  stores: storesReducer,
   products: productsReducer,
+  users: usersReducer,
 });
