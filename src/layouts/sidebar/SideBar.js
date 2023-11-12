9bf3cd91e4094f9bcc47c3345afac5a489c63c04
Minister <mundestephane13@gmail.com>
2023-11-12 19:12:58 +0100

    Added icons to the sidebar

--- src/layouts/sidebar/SideBar.js
+++ src/layouts/sidebar/SideBar.js
@@ -2,14 +2,20 @@ import React, { useMemo } from "react";
 import { SearchWidget } from "../../components/searchwiget";
 import { SearchURLContext } from "../../contexts";
 import SideBarItem from "./SideBarItem";
+import financesIcon from "../../assets/icons/chart-mixed-svgrepo-com.svg";
+import listingsIcon from "../../assets/icons/Group 191.svg";
+import ordersIcon from "../../assets/icons/memo-svgrepo-com.svg";
+import marketingIcon from "../../assets/icons/marketing-svgrepo-com.svg";
+import dashboardIcon from "../../assets/icons/course-tile-sort-svgrepo-com.svg";
+import settingIcon from "../../assets/icons/Icon feather-settings.svg";
 
 const items = [
-  { title: "dashboard", to: "/dashboard" },
-  { title: "listings", to: "/listings" },
-  { title: "orders", to: "/orders" },
-  { title: "finances", to: "/finances" },
-  { title: "marketing", to: "/marketing" },
-  { title: "settings", to: "/settings" },
+  { title: "dashboard", to: "/dashboard", icon: dashboardIcon },
+  { title: "listings", to: "/listings", icon: listingsIcon },
+  { title: "orders", to: "/orders", icon: ordersIcon },
+  { title: "finances", to: "/finances", icon: financesIcon },
+  { title: "marketing", to: "/marketing", icon: marketingIcon },
+  { title: "settings", to: "/settings", icon: settingIcon },
 ];
 
 const SideBar = () => {
@@ -28,7 +34,12 @@ const SideBar = () => {
       </div>
       <div className="sidebar__menu">
         {items.map((item, index) => (
-          <SideBarItem key={index + 1} title={item.title} to={item.to} />
+          <SideBarItem
+            key={index + 1}
+            title={item.title}
+            to={item.to}
+            icon={item.icon}
+          />
         ))}
       </div>
     </div>
--- src/layouts/sidebar/SideBarItem.js
+++ src/layouts/sidebar/SideBarItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { SvgIcon } from "@mui/material";
 
 /**
  * @breif Render side navigation
@@ -15,8 +16,11 @@ const SideBarItem = ({ icon, title, to }) => {
     <Link
       to={to}
       className={`sidebar__item ${pathname === to ? "active" : ""}`}
+      style={{ alignItems: "center", display: "flex" }}
     >
-      <span className="sidebar__icon"></span>
+      <span className="sidebar__icon me-1">
+        <img src={icon} alt="icon" style={{ height: 20, width: 20 }} />
+      </span>
       <span className="sidebar__title">{title}</span>
       <span className="sidebar__line"></span>
     </Link>
