3629d11508048f707b6db9b17a49702f2d3affb7
tchoforwilson <tchoforwilson@gmail.com>
2023-11-22 05:12:23 +0100

    Follow store button refactored to show follow and unfollow

--- src/components/buttons/FollowButton.js
+++ src/components/buttons/FollowButton.js
@@ -2,14 +2,15 @@ import React from "react";
 
 /**
  * @breif Render a follow button
+ * @param {Boolean} isFollowing Text for the following of store
  * @param {Function} onClick Handler when button is clicked
  * @returns {JSX}
  */
-const FollowButton = ({ onClick }) => {
+const FollowButton = ({ isFollowing = false, onClick }) => {
   const handleClick = () => onClick;
   return (
     <button className="btn btn-follow" onClick={handleClick}>
-      follow this store
+      {isFollowing ? " follow this store" : "unfollow"}
     </button>
   );
 };
