2be849a0dee4bc42148a22be68ae4d14e374e0a6
tchoforwilson <tchoforwilson@gmail.com>
2023-11-22 05:11:15 +0100

    Added notification modal

--- src/layouts/navbar/NavBar.js
+++ src/layouts/navbar/NavBar.js
@@ -4,15 +4,19 @@ import { Link } from "react-router-dom";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+import { IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { SearchWidget } from "../../components/searchwiget";
+import { Notifications } from "../../components/notifications";
 import NavLink from "./NavLink";
 import { CartDropdown } from "../../components/dropdown";
 import { NavBarContext } from "../../contexts";
+import notifications from "./notifications";
 
 const NavBar = ({ onSearch }) => {
   const [isSticky, setIsSticky] = useState(false);
   const [showBag, setShowBag] = useState(false);
+  const [showNotification, toggleNotification] = useState(false);
   const { isOpen, toggleNavBar } = useContext(NavBarContext);
 
   const handleSearch = () => onSearch();
@@ -46,19 +50,32 @@ const NavBar = ({ onSearch }) => {
       {/* Private links */}
       <div className="nav-profile">
         <div className="nav-profile__cart">
-          <ShoppingBagOutlinedIcon
-            className="nav-profile__icon"
+          <IconButton
+            className="nav-profile__btn"
             onClick={() => setShowBag(!showBag)}
-          />
+          >
+            <ShoppingBagOutlinedIcon className="nav-profile__icon" />
+          </IconButton>
           <span>1 item</span>
           <CartDropdown active={showBag} />
         </div>
         <div className="nav-profile__notification">
-          <NotificationsOutlinedIcon className="nav-profile__icon" />
+          <IconButton
+            className="nav-profile__btn"
+            onClick={() => toggleNotification(!showNotification)}
+          >
+            <NotificationsOutlinedIcon className="nav-profile__icon" />
+          </IconButton>
           <span className="nav-profile__badge">12</span>
+          <Notifications
+            notifications={notifications}
+            active={showNotification}
+          />
         </div>
         <Link className="nav-profile__user" to="/profile">
-          <AccountCircleOutlinedIcon className="nav-profile__icon" />
+          <IconButton className="nav-profile__btn">
+            <AccountCircleOutlinedIcon className="nav-profile__icon" />
+          </IconButton>
         </Link>
       </div>
     </nav>
