7ba91d30a4e7d9af1fa177a3b0b915a4fa511e45
tchoforwilson <tchoforwilson@gmail.com>
2023-11-25 17:54:49 +0100

    Implemented Error context

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -5,3 +5,4 @@ export const SideBarContext = React.createContext();
 export const SearchURLContext = React.createContext();
 export const TableContext = React.createContext();
 export const OpenAddProductModalContext = React.createContext();
+export const ErrorContext = React.createContext();
