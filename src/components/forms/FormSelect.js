2532da7412a1dbd413fad2af1df843751ebdbe4a
tchoforwilson <tchoforwilson@gmail.com>
2023-11-26 19:55:16 +0100

    Implemented default value

--- src/components/forms/FormSelect.js
+++ src/components/forms/FormSelect.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useFormikContext } from "formik";
+import _ from "lodash";
 import FormErrorMessage from "./FormErrorMessage";
 import { AppSelect } from "../inputs";
+import { CONST_ZEROU } from "../../utilities/constants";
 
 /**
  * @breif Render a form select field
@@ -14,14 +16,34 @@ import { AppSelect } from "../inputs";
 const FormSelect = ({ name, items, multiple = false, isBordered = false }) => {
   const { values, errors, touched, setFieldValue, setFieldTouched } =
     useFormikContext();
+
+  const [defaultValue, setDefaultValue] = useState("");
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setFieldValue(name, value);
+    setDefaultValue(value);
+  };
+
+  useEffect(() => {
+    if (items.lenght > CONST_ZEROU) {
+      if (!_.get(values, name)) {
+        setDefaultValue(items[CONST_ZEROU]);
+        setFieldValue(name, items[CONST_ZEROU]);
+      } else {
+        setDefaultValue(_.get(values, name));
+        setFieldValue(name, _.get(values, name));
+      }
+    }
+  }, [name, values, items, setFieldValue]);
   return (
     <div className="form-select">
       <AppSelect
         name={name}
         items={items}
         multiple={multiple}
-        value={values[name]}
-        onChange={(e) => setFieldValue(name, e.target.value)}
+        value={defaultValue}
+        onChange={(e) => handleChange(e)}
         onBlur={() => setFieldTouched(name)}
         className={`form-select__input ${
           isBordered ? "form-select__input--bordered" : ""
