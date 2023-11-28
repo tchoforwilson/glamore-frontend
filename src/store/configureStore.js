a07f604042e0cc90f58873ef7f8937996fc4da64
Minister <mundestephane13@gmail.com>
2023-11-28 15:51:28 +0100

    Added the middleware for the products api to the store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiRequest, logger, toastify } from "./middlewares";
 import reducer from "./reducer";
+import productsApi from "./entities/products.api";
 
 const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(logger, apiRequest, toastify),
+    getDefaultMiddleware().concat(
+      logger,
+      apiRequest,
+      toastify,
+      productsApi.middleware,
+    ),
 });
 
 export default store;
