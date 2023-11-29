925fd21e89644b815f7da2b6c2e085735dbfd96a
Minister <mundestephane13@gmail.com>
2023-11-29 17:42:40 +0100

    Updated combined reducer to work with the products api

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auth";
 import entitiesReducer from "./entities";
+import productsApi from "./entities/products.api";
 
 export default combineReducers({
   entities: entitiesReducer,
   auth: authReducer,
+  [productsApi.reducerPath]: productsApi.reducer,
 });
