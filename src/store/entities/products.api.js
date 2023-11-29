456900cdd2901b5c0767b35300fd90ac2174a1dc
Minister <mundestephane13@gmail.com>
2023-11-29 17:43:13 +0100

    added clean comments to the productApi slice

--- src/store/entities/products.api.js
+++ src/store/entities/products.api.js
@@ -1,38 +1,56 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { API_URL } from "../../configurations/config";
+
 const productsApi = createApi({
   // The path the will be used as name in the redux store
   reducerPath: "products",
 
   // the base query for all the endpoints
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
 
-  // Strings representing the various models the
+  // Strings representing the various models the current query interacts with
   tagTypes: ["Product"],
 
   // The various endpoints from which the frontend will interact with
   endpoints: (builder) => ({
     // Enpoint to fetch all products
     getProducts: builder.query({
-      query: () => ({
+      query: (params) => ({
         url: "/products",
         method: "GET",
+        params,
       }),
-      providesTags: ["Product"],
+      providesTags: (result) => {
+        return result
+          ? [
+              ...result?.data.map((product) => ({
+                type: "Product",
+                id: product.id,
+              })),
+              { type: "Product", id: "LIST" },
+            ]
+          : [{ type: "Product", id: "LIST" }];
+      },
     }),
 
+    // Get a single product from the database using it id
     getProduct: builder.query({
       query: (id) => `products/${id}`,
-      providesTags: ["Product"],
+      providesTags: ["Product"], // Tells the query that it should provide the "Product" tag and should be autorefetched on invalidation by another endpoint
+    }),
+
+    // Create a product
+    createProduct: builder.mutation({
+      query: ({ shopId, ...filters }) => ({
+        url: "/products",
+        method: "POST",
+        body: { shopId, filters },
+      }),
+      invalidateTags: ["Product"], // Tells the query that this endpoint is going to modify a "Product" from the backend
     }),
   }),
 });
 
 export const { useGetProductsQuery, useGetProductQuery } = productsApi;
-
-export const {
-  reducer: productsReducer,
-  middleware: ProductsMiddleware,
-  reducerPath: ProductsReducerPath,
-} = productsApi;
 
 export default productsApi;
