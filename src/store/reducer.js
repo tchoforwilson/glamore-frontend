b9b2f80fd96fe53ff81cd5cb54dc40127f9c012a
Minister <mundestephane13@gmail.com>
2023-12-04 17:04:50 +0100

    added authentication api to the store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,10 +3,12 @@ import authReducer from "./auth";
 import entitiesReducer from "./entities";
 import productsApi from "./entities/products.api";
 import categoriesApi from "./entities/categories.api";
+import authApi from "./entities/auth.api";
 
 export default combineReducers({
   entities: entitiesReducer,
   auth: authReducer,
   [productsApi.reducerPath]: productsApi.reducer,
-  [categoriesApi.reducerPath]: categoriesApi.reducer
+  [categoriesApi.reducerPath]: categoriesApi.reducer,
+  [authApi.reducerPath]: authApi.reducer,
 });
