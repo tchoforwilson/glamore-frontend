85c464f3fcf8a5545545d70ef2dc09f6cda879e2
Minister <mundestephane13@gmail.com>
2023-12-27 00:18:02 +0100

    Added the article card to the list of cards to be exported

---
+++ src/components/cards/ArticleCard.js
@@ -0,0 +1,8 @@
+export default function ArticleCard() {
+  return (
+    <article className="article-card">
+      <div>This is an inner dic</div>
+      This is an article card
+    </article>
+  );
+}
--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,3 +1,4 @@
-export { default as ProductCard } from './ProductCard';
-export { default as CategoryCard } from './CategoryCard';
-export { default as PreviewCard } from './PreviewCard';
+export { default as ProductCard } from "./ProductCard";
+export { default as CategoryCard } from "./CategoryCard";
+export { default as PreviewCard } from "./PreviewCard";
+export { default as ArticleCard } from "./ArticleCard";
