4c2254a437941ebba6c62b6546f4e6fc7e45a752
Minister <mundestephane13@gmail.com>
2023-12-27 02:37:25 +0100

    Update tailwind's content for purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,css,scss,sass}"],
+  content: ["./src/**/*.{js,jsx,css,scss,sass,svg}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "320px",
+        tablet: "540px",
+        laptop: "1024px",
+        desktop: "1280px",
+        tv: "1980px",
+      },
+    },
   },
   plugins: [],
 };
