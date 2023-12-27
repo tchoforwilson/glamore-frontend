ded50ed51713c15d3e2a63c18bf38e4df83649b6
Minister <mundestephane13@gmail.com>
2023-12-27 03:07:27 +0100

    Refactored the Article card

--- src/components/cards/ArticleCard.js
+++ src/components/cards/ArticleCard.js
@@ -1,8 +1,17 @@
-export default function ArticleCard() {
+import styles from "./ArticleCard.module.scss";
+
+export default function ArticleCard({ article, className = "" }) {
   return (
-    <article className="article-card">
-      <div>This is an inner dic</div>
-      This is an article card
+    <article className={`${styles["article-card"]} ${className}`}>
+      <div className={styles["article-card__inner"]}>
+        <img
+          src={article.image}
+          alt={article.title}
+          className="object-cover object-center size-full"
+        />
+      </div>
+      <div className={styles["article-card__overlay"]}></div>
+      <h3 className={styles["article-card__title"]}>{article.title}</h3>
     </article>
   );
 }
