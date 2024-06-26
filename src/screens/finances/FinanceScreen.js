95e88afb4c2991db36b199181599c7adb4a57c7a
tchoforwilson <tchoforwilson@gmail.com>
2024-06-26 09:48:03 +0100

    Changed layout to grid template

--- src/screens/finances/FinanceScreen.js
+++ src/screens/finances/FinanceScreen.js
@@ -2,103 +2,82 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Box, Grid } from "@mui/material";
 import { StoreScreenLayout } from "../../layouts";
-import FinanceItem from "./FinanceItem";
+import Finance from "./Finance";
+import Fees from "./Fees";
 import { numberFormatter } from "../../utilities/functions";
 
 const FinanceScreen = () => {
   return (
     <StoreScreenLayout title="Finances & Overview">
-      <Box className="store-content">
-        <Link to="finace-report" className="finance-report">
-          download finance report
-        </Link>
-        <Grid container spacing={3}>
-          <Grid item md={4} sm={12}>
-            <FinanceItem
-              title="Discount sales"
-              figure={numberFormatter.format(52)}
-              description="FCFA"
-            >
-              <Link to="/cash-out" className="finance-item__button">
-                cashout
-              </Link>
-            </FinanceItem>
-            <FinanceItem
-              title="Discount sales"
-              figure={numberFormatter.format(52)}
-              description="Items"
-            />
-
-            <Grid container spacing={2}>
-              <Grid item sx={6}>
-                <FinanceItem
-                  title="Positive reviews"
-                  figure={numberFormatter.format(163)}
-                />
-              </Grid>
-              <Grid item sx={6}>
-                <FinanceItem
-                  title="Negative reviews"
-                  figure={numberFormatter.format(4)}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item md={4} sm={12}>
-            <FinanceItem
-              title="Average customer order amount"
-              figure={numberFormatter.format(10600)}
-              description="FCFA"
-            />
-            <FinanceItem
-              title="Average recurring customers"
-              figure={numberFormatter.format(251)}
-              description="People"
-            />
-            <FinanceItem
-              title="New customers"
-              figure={numberFormatter.format(250)}
-            >
-              <p>
-                Since January 31<sub>st</sub> 2023
-              </p>
-            </FinanceItem>
-          </Grid>
-          <Grid item md={4} sm={12}>
-            <div className="h-100 d-flex flex-column justify-content--between">
-              <div className="fees commission">
-                <p className="fees__title">Commission fees</p>
-                <h2 className="fees__percentage">15%</h2>
-                <div className="fees__amount">
-                  {numberFormatter.format(112185.5)}
-                  <span className="fees__currency">FCFA</span>
-                </div>
-              </div>
-              <div className="fees advertisement">
-                <p className="fees__title">Advertisement fees</p>
-                <div className="fees__amount">
-                  {numberFormatter.format(112, 185.5)}
-                  <span className="fees__currency">FCFA</span>
-                </div>
-                <p className="fees__text">
-                  {numberFormatter.format(6520)} advertized products since June
-                  16th 2023
-                </p>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-        <Grid container>
-          <div className="account-overview">
-            <h2>Account overview</h2>
-            <p>
-              Your account has reached 20% more people in the last 24 hours,
-              also, 16 of your products have been saved by customers. Overal
-              your store has grown by 30% in the last 8 days. Congratulations!
-            </p>
-          </div>
-        </Grid>
-      </Box>
+      <Link to="finace-report" className="finance-report">
+        download finance report
+      </Link>
+      <div className="store-content finance-content">
+        <Finance
+          title="Discount sales"
+          figure={numberFormatter.format(52)}
+          description="FCFA"
+          className="finance finance--1"
+        >
+          <Link to="/cash-out" className="finance__button">
+            cashout
+          </Link>
+        </Finance>
+        <Finance
+          title="Average customer order amount"
+          figure={numberFormatter.format(10600)}
+          description="FCFA"
+          className="finance finance--2"
+        />
+        <Fees
+          title="Commission fees"
+          amount={112185.5}
+          percentage={15}
+          className="fees commission"
+        />
+        <Finance
+          title="Discount sales"
+          figure={numberFormatter.format(52)}
+          description="Items"
+          className="finance finance--3"
+        />
+        <Finance
+          title="Average recurring customers"
+          figure={numberFormatter.format(251)}
+          description="People"
+          className="finance finance--4"
+        />
+        <Finance
+          title="Positive reviews"
+          figure={numberFormatter.format(163)}
+          className="finance finance--5"
+        />
+        <Finance
+          title="New customers"
+          figure={numberFormatter.format(250)}
+          className="finance finance--7"
+        />
+        <Finance
+          title="Negative reviews"
+          figure={numberFormatter.format(4)}
+          className="finance finance--8"
+        />
+        <Fees
+          title="Advertisement fees"
+          amount={112185.5}
+          quantity={6520}
+          description="advertized products since June 16th 2023"
+          className="fees advertisement"
+        />
+        <div className="account-overview">
+          <h2>Account overview</h2>
+          <p>
+            Your account has reached 20% more people in the last 24 hours, also,
+            16 of your products have been saved by customers. Overal your store
+            has grown by 30% in the last 8 days. Congratulations!
+          </p>
+        </div>
+      </div>
     </StoreScreenLayout>
   );
 };
